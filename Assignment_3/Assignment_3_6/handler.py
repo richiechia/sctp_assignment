c68473c0caed7b306dd022f5854cd823775d053f
richiechia <richiechia@gmail.com>
2024-02-06 21:42:38 +0800

    Added python api

---
+++ Assignment_3/Assignment_3_6/handler.py
@@ -0,0 +1,13 @@
+
+import json
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    payload = json.dumps(event, indent=2)
+    print("Received event: " + payload)
+    return {
+        'statusCode' : 200,
+        'body': 'hello world'
+    }
+
